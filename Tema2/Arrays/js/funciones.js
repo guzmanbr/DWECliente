f9a71b51ee9ad6bf0152b3f57ded655dbf76381b
guzmanbr <guzmanghu@gmail.com>
2023-10-16 18:58:51 +0200

    Clase

---
+++ Tema2/Arrays/js/funciones.js
@@ -0,0 +1,17 @@
+//Arrays
+
+// let a = new Array(); 
+// let b = new Array(2,4,6); 
+
+// let a = [];
+// let b = [2, 4, 6];
+
+let a = ['lunes', 'martes', 2, 4, 6]
+
+a[7] = 'Juan' // ahora a = ['Lunes', 'Martes', 2, 4, 6, , , 'Juan']
+console.log(a[5]) // imprime indefinido
+let b = 6;
+console.log(b[0]);
+b="pepe";
+console.log(b[2]); // imprime undefined
+console.log(b[0]);
