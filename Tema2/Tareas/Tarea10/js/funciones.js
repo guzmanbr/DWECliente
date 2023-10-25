7330b4569a3e649a9ca656f6a2a5efafee3c23e4
guzmanbr <guzmanghu@gmail.com>
2023-10-25 18:34:04 +0200

    inicioClase

---
+++ Tema2/Tareas/Tarea10/js/funciones.js
@@ -0,0 +1,36 @@
+
+/*
+Se pide crear unas clases con las propiedades y métodos necesarios para lograr el objetivo final.
+Hay que tener en cuenta los siguientes aspectos:
+• Habrá 3 clases: Persona, Empleado y Cliente. Empleado y Cliente son clases heredadas de Persona.
+
+• Los objetos de la clase Persona tienen un identificador (atributo id) que será numérico, auto incremental, 
+comenzando en 101.
+
+• Los objetos de la clase Empleado tendrán un id que comenzará en 201.
+
+• Los objetos de la clase Cliente tendrán un id que comenzará en 301.
+
+• Todos los objetos creados pasarán por el constructor de la clase padre. Por lo que el atributo id de la clase 
+Persona se verá incrementado al crear un nuevo objeto de cualquier clase.
+
+• Todas las clases incorporan su método toString().
+
+• Los nombres y apellidos, aunque se guarden en mayúsculas o minúsculas, se devolverán en formato “nombre 
+propio”: Primera letra de cada palabra en mayúscula y las siguientes en minúscula.
+
+• La fecha se devolverá en el formato local del navegador.
+
+• El sueldo de los empleados se devolverá en formato de moneda local.
+
+• La fecha de registro de los clientes será la fecha actual, aunque habrá método para cambiarla.
+
+• Se podrán crear objetos en base a objetos de la clase persona, como en la siguiente línea:
+
+• Habrá una constante en la clase Persona que establezca la cantidad máxima de objetos de la clase Persona.
+
+• La salida por consola de los métodos toString() será como en la figura de abajo. Para facilitar esta tarea se 
+utilizará Template String
+
+*/
+
