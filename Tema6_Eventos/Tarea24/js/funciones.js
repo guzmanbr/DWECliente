fdd484b342c93d282e63fa3a581426de0abdad68
guzmanbr <guzmanghu@gmail.com>
2023-12-20 19:21:20 +0100

    calse

--- Tema6_Eventos/Tarea24/js/funciones.js
+++ Tema6_Eventos/Tarea24/js/funciones.js
@@ -10,10 +10,14 @@ function generarColorAleatorio() {
 function cambiarColor(input1,input2) {
     input1.style.backgroundColor=generarColorAleatorio();
     input2.style.backgroundColor=generarColorAleatorio();
+    input1.value="Hola";
+    input2.value="Adios";
 }
+
 
 //localizo los dos inputs
 let input1 =document.getElementById('input1');
 let input2 =document.getElementById('input2');
+
 //asigno el evento al boton y hago el bindeo
 document.getElementById('boton').addEventListener('click',cambiarColor.bind("",input1,input2));
